1e8234dc8f782e9fe8a6ade7a24dc277b316a632
PranayIngole17 <121795110+PranayIngole17@users.noreply.github.com>
2024-10-26 06:02:26 +0000

    parctise branch changes

---
+++ practice.js
@@ -0,0 +1,16 @@
+
+// let b = "d"
+// let a = `pranay ${b} ingole`
+// // console.log(a.toUpperCase())
+// // console.log(a.length)
+
+// // console.log(a.includes("ingole"))
+
+// console.log("")
+
+const a = [1,2,3,4,5,6]
+
+
+const b=a.splice(1,3)
+console.log(b)
+console.log(a)
